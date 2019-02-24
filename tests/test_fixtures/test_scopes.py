02c855687b57effbc71bc1914c48cf0022ba4ac3
florimondmanca <florimond.manca@gmail.com>
2019-02-24 19:53:35 +0100

    test that session is the default scope

--- tests/test_fixtures/test_scopes.py
+++ tests/test_fixtures/test_scopes.py
@@ -1,6 +1,14 @@
 from bocadillo.fixtures import Store
 
 
+def test_fixtures_are_session_scoped_by_default(store: Store):
+    @store.fixture
+    def items():
+        pass
+
+    assert items.scope == "session"
+
+
 def test_session_fixture_is_recomputed_every_time(store: Store):
     @store.fixture(scope="session")
     def items():
