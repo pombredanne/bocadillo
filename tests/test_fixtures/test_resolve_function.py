77b8491aa439af096843d360811ed246d6829103
florimondmanca <florimond.manca@gmail.com>
2019-02-24 19:55:24 +0100

    use store instead of resolver in tests for resolve_function

--- tests/test_fixtures/test_resolve_function.py
+++ tests/test_fixtures/test_resolve_function.py
@@ -2,28 +2,26 @@ import inspect
 
 import pytest
 
-from bocadillo.fixtures import Store, Resolver
+from bocadillo.fixtures import Store
 
 
-def test_resolve_for_func_returns_function(resolver: Resolver):
-    func = resolver.resolve_function(lambda: "test")
+def test_resolve_for_func_returns_function(store: Store):
+    func = store.resolve_function(lambda: "test")
     assert inspect.isfunction(func)
 
 
-def test_if_no_fixture_declared_then_behaves_like_func(resolver: Resolver):
-    func = resolver.resolve_function(lambda: "test")
+def test_if_no_fixture_declared_then_behaves_like_func(store: Store):
+    func = store.resolve_function(lambda: "test")
     assert func() == "test"
 
 
-def test_if_fixture_does_not_exist_then_missing_argument(
-    store: Store, resolver: Resolver
-):
+def test_if_fixture_does_not_exist_then_missing_argument(store: Store):
     @store.fixture
     def gra():
         return "gra"
 
     # "gra" exists, but not "arg"
-    func = resolver.resolve_function(lambda arg: 2 * arg)
+    func = store.resolve_function(lambda arg: 2 * arg)
 
     with pytest.raises(TypeError):
         func()
@@ -31,10 +29,9 @@ def test_if_fixture_does_not_exist_then_missing_argument(
     assert func(10) == 20
 
 
-def test_if_fixture_exists_then_injected(store: Store, resolver: Resolver):
+def test_if_fixture_exists_then_injected(store: Store):
     @store.fixture
     def arg():
         return "foo"
 
-    func = resolver.resolve_function(lambda arg: 2 * arg)
-    assert func() == "foofoo"
+    assert store.resolve_function(lambda arg: 2 * arg)() == "foofoo"
